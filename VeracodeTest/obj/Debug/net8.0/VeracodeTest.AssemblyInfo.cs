ab986b16a1f0165870e958d0fe66c44b54866b36
Deeks-63 <Deekshant.goyal@ecolab.com>
2024-08-26 10:37:01 +0530

    Adding the Upload and scan workflow

--- VeracodeTest/obj/Debug/net8.0/VeracodeTest.AssemblyInfo.cs
+++ VeracodeTest/obj/Debug/net8.0/VeracodeTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VeracodeTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43946fc15db4d926b195541d74bcee210459407a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8279f2381e676b9e27e843df8975e3d878d633c")]
 [assembly: System.Reflection.AssemblyProductAttribute("VeracodeTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VeracodeTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
